a62688306c9b66f192cc96796d6ec7af107353fa
jaewan <stpjw777@gmail.com>
2021-02-22 15:47:56 +0900

    state update

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -10,15 +10,24 @@ class Habits extends Component {
     ],
   };
   handleIncrement = habit => {
-    console.log(`handleIncrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
   };
 
   handleDecrement = habit => {
-    console.log(`handleDecrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    // 좋지 못한 코드
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
   };
 
   handleDelete = habit => {
-    console.log(`handleDelete ${habit.name}`);
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({ habits });
   };
 
   render() {
